95c8c4cd1949dde2780e1eb27031c9017ef2eb15
Thiago Ferraz de Lima <72419242+ferraztfl@users.noreply.github.com>
2026-09-08 07:28:50 -0300

    feat: wire published question lookup service

--- src/modules/question-bank/infrastructure/composition/question-bank-application.ts
+++ src/modules/question-bank/infrastructure/composition/question-bank-application.ts
@@ -1,8 +1,15 @@
+import { GetPublishedQuestionByIdUseCase } from "../../application/use-cases/get-published-question-by-id";
 import { ListPublishedQuestionsUseCase } from "../../application/use-cases/list-published-questions";
 import { PrismaQuestionRepository } from "../repositories/prisma-question-repository";
 
 export function createListPublishedQuestionsUseCase(): ListPublishedQuestionsUseCase {
   return new ListPublishedQuestionsUseCase(
+    new PrismaQuestionRepository(),
+  );
+}
+
+export function createGetPublishedQuestionByIdUseCase(): GetPublishedQuestionByIdUseCase {
+  return new GetPublishedQuestionByIdUseCase(
     new PrismaQuestionRepository(),
   );
 }
