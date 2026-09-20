9bba0d0d0a1929a6a623be27b167497e4afcf65d
Thiago Ferraz de Lima <72419242+ferraztfl@users.noreply.github.com>
2026-09-20 06:34:40 -0300

    feat: compose question review application

--- src/modules/question-bank/infrastructure/composition/question-bank-application.ts
+++ src/modules/question-bank/infrastructure/composition/question-bank-application.ts
@@ -1,18 +1,30 @@
 import {
+  AssignQuestionTopicUseCase,
+} from "../../application/use-cases/assign-question-topic";
+import {
   GetPublishedQuestionByIdUseCase,
 } from "../../application/use-cases/get-published-question-by-id";
 import {
   ListPublishedQuestionsUseCase,
 } from "../../application/use-cases/list-published-questions";
 import {
+  ListQuestionsForReviewUseCase,
+} from "../../application/use-cases/list-questions-for-review";
+import {
   ListQuestionExplorerFacetsUseCase,
 } from "../../application/use-cases/list-question-explorer-facets";
+import {
+  PublishReviewedQuestionUseCase,
+} from "../../application/use-cases/publish-reviewed-question";
 import {
   SubmitPublishedQuestionAnswerUseCase,
 } from "../../application/use-cases/submit-published-question-answer";
 import {
   PrismaPublicQuestionReadRepository,
 } from "../repositories/prisma-public-question-read-repository";
+import {
+  PrismaQuestionReviewRepository,
+} from "../repositories/prisma-question-review-repository";
 import {
   PrismaQuestionRepository,
 } from "../repositories/prisma-question-repository";
@@ -38,5 +50,24 @@ export function createListQuestionExplorerFacetsUseCase(): ListQuestionExplorerF
 export function createSubmitPublishedQuestionAnswerUseCase(): SubmitPublishedQuestionAnswerUseCase {
   return new SubmitPublishedQuestionAnswerUseCase(
     new PrismaQuestionRepository(),
+  );
+}
+
+
+export function createListQuestionsForReviewUseCase(): ListQuestionsForReviewUseCase {
+  return new ListQuestionsForReviewUseCase(
+    new PrismaQuestionReviewRepository(),
+  );
+}
+
+export function createAssignQuestionTopicUseCase(): AssignQuestionTopicUseCase {
+  return new AssignQuestionTopicUseCase(
+    new PrismaQuestionReviewRepository(),
+  );
+}
+
+export function createPublishReviewedQuestionUseCase(): PublishReviewedQuestionUseCase {
+  return new PublishReviewedQuestionUseCase(
+    new PrismaQuestionReviewRepository(),
   );
 }
